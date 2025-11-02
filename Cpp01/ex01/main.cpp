0ddabfb883e0616ec224a00373b463f8fa702b5a
Maxime <mdsiurds@gmail.com>
2025-11-02 20:40:08 +0100

    news leaks

--- Cpp01/ex01/main.cpp
+++ Cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 16:35:25 by max               #+#    #+#             */
-/*   Updated: 2025/11/02 20:24:20 by max              ###   ########.fr       */
+/*   Updated: 2025/11/02 20:33:27 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(){
     horde[2].announce();
     horde[4].announce();
     (void) horde;
-    //delete[] horde;
+    //delete[] horde; //for the leaks take off the comment
     std::cout << "End of program, all zombies should be destroyed with fire now." << std::endl;
     return 0;
 }
