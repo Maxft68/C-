76db3fdf89107fc9d8e84bf4ed0191d19c948f3a
Maxime <mdsiurds@gmail.com>
2025-11-17 17:52:06 +0100

    ex06 fini

--- Cpp01/ex06/main.cpp
+++ Cpp01/ex06/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+        */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 14:45:54 by mdsiurds          #+#    #+#             */
-/*   Updated: 2025/11/17 15:46:57 by mdsiurds         ###   ########.fr       */
+/*   Updated: 2025/11/17 17:51:50 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int main(int argc, char **argv){
 	
 	if (argc != 2){
-		std::cout << "./harlFilter [Argument]" << std::endl;
+		std::cout << "Error, use like this: ./harlFilter [Argument]" << std::endl;
 		return(1);
 	}
-		
-	Harl couille;
+
+	Harl harl;
 	std::string filter[5] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	
 	
@@ -27,13 +27,16 @@ int main(int argc, char **argv){
 		if (argv[1] == filter[i] || i == 4){
 			switch(i){
 				case 0:
-					couille.complain("DEBUG");
+					harl.complain("DEBUG");
+					/* fallthrough */
 				case 1:
-					couille.complain("INFO");
+					harl.complain("INFO");
+					/* fallthrough */
 				case 2:
-					couille.complain("WARNING");
+					harl.complain("WARNING");
+					/* fallthrough */
 				case 3:
-					couille.complain("ERROR");
+					harl.complain("ERROR");
 					break;
 				default:
 					std::cout << "What you say?" << std::endl;
@@ -41,34 +44,7 @@ int main(int argc, char **argv){
 			}
 		}
 	}
-	
-	
 
 	return(0);
 }
 
-// 	std::string a = "coucou";
-// 	std::string b = "ABSOLUMEBT PAS";
-// 	std::string c = "bla";
-// 	std::string d = "BLA";
-
-// 	std::string name[5] = {"coucou", "non", "oui", "bla"};
-
-
-// 	int i = 0;
-// 	while(i < 5){
-// 		if (name[i] == a)
-// 			break;
-// 		i++;
-// 	}
-// 	switch (i){
-// 		case 0:
-// 			std::cout << "bravo" << std::endl;
-// 			break;
-// 		case 1:
-// 			std::cout << "gg" << std::endl;
-// 			break;
-// 		default:
-// 			std::cout << "fuck" << std::endl;
-// 	}
-// }
