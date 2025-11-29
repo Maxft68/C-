6e748e4cb2404d0346dc6053e5c61e3f03a217b9
Maxime <mdsiurds@gmail.com>
2025-11-29 18:11:46 +0100

    up

--- Cpp00/ex02/Account.hpp
+++ Cpp00/ex02/Account.hpp
@@ -1,58 +1,56 @@
-// ************************************************************************** //
-//                                                                            //
-//                Account.hpp for GlobalBanksters United                //
-//                Created on  : Thu Nov 20 19:43:15 1989                      //
-//                Last update : Wed Jan 04 14:54:06 1992                      //
-//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
-//                                                                            //
-// ************************************************************************** //
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 18:09:45 by max               #+#    #+#             */
+/*   Updated: 2025/11/29 18:10:24 by max              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 
 #pragma once
 #ifndef __ACCOUNT_H__
 #define __ACCOUNT_H__
 
-// ************************************************************************** //
-//                               Account Class                                //
-// ************************************************************************** //
-
 class Account {
 
+	public:
 
-public:
+		typedef Account		t;
 
-	typedef Account		t;
+		static int	getNbAccounts( void );
+		static int	getTotalAmount( void );
+		static int	getNbDeposits( void );
+		static int	getNbWithdrawals( void );
+		static void	displayAccountsInfos( void );
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
-	static void	displayAccountsInfos( void );
+		Account( int initial_deposit );
+		~Account( void );
 
-	Account( int initial_deposit );
-	~Account( void );
-
-	void	makeDeposit( int deposit );
-	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;
+		void	makeDeposit( int deposit );
+		bool	makeWithdrawal( int withdrawal );
+		int		checkAmount( void ) const;
+		void	displayStatus( void ) const;
 
 
-private:
+	private:
 
-	static int	_nbAccounts;
-	static int	_totalAmount;
-	static int	_totalNbDeposits;
-	static int	_totalNbWithdrawals;
+		static int	_nbAccounts;
+		static int	_totalAmount;
+		static int	_totalNbDeposits;
+		static int	_totalNbWithdrawals;
 
-	static void	_displayTimestamp( void );
+		static void	_displayTimestamp( void );
 
-	int				_accountIndex;
-	int				_amount;
-	int				_nbDeposits;
-	int				_nbWithdrawals;
+		int				_accountIndex;
+		int				_amount;
+		int				_nbDeposits;
+		int				_nbWithdrawals;
 
-	Account( void );
+		Account( void );
 
 };
 
