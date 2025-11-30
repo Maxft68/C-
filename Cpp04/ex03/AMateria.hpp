d53e204f5735ed134e0d537be2595c15182f118b
Maxime <mdsiurds@gmail.com>
2025-11-30 21:35:38 +0100

    big boss en cours

---
+++ Cpp04/ex03/AMateria.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 21:08:39 by max               #+#    #+#             */
+/*   Updated: 2025/11/30 21:24:45 by max              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+#define RED_RESET(str) RED << (str) << RESET
+#define GREEN_RESET(str) GREEN << (str) << RESET
+
+class ICharacter;
+
+class AMateria{
+	protected:
+		std::string _type;
+	public:
+		AMateria();
+		virtual ~AMateria();
+		AMateria(const AMateria& other);
+		AMateria(std::string const & type);
+
+		AMateria& operator=(const AMateria& other);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
