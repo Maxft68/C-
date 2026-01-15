b859cca21b1c2877a82f4dcb242177d0ed3e9d37
Maxft68 <mdsiurds@gmail.com>
2026-01-15 14:58:55 +0100

    go

--- Cpp08/ex01/Span.cpp
+++ Cpp08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:55:21 by mdsiurds          #+#    #+#             */
-/*   Updated: 2026/01/15 14:56:47 by mdsiurds         ###   ########.fr       */
+/*   Updated: 2026/01/15 14:58:48 by mdsiurds         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void Span::addNumber(int n){
     std::deque<int>::iterator end = _Container.end();
     std::deque<int>::iterator begin = _Container.begin();
 
-    if (_Container.size == _N)
+    if (_Container.size() == _N)
         throw NotEnoughPlace;
-    _Container.back(n);
+    _Container.back(n); //voir cplusplus doc deque
 }
