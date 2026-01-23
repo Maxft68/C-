87c53d954801041363181023d081e326d737bf03
Maxft68 <mdsiurds@gmail.com>
2026-01-23 09:27:40 +0100

    ok

--- Cpp09/ex00/BitcoinExchange.cpp
+++ Cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:20:03 by mdsiurds          #+#    #+#             */
-/*   Updated: 2026/01/23 09:04:06 by mdsiurds         ###   ########.fr       */
+/*   Updated: 2026/01/23 09:05:48 by mdsiurds         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,8 @@ void BitcoinExchange::parse(const std::string &str){
     while (std::getline(file, str_csv)){
         std::string date = str_csv.substr(0, 10);
         std::string price = str_csv.substr(11, str_csv.length() - 1);
-        // std::cout << "date: " << date << std::endl;
-        // std::cout << "price: " << price << std::endl;
         _Data[date] = strtod(price.c_str(), NULL);
         //std::cout << "verif dans map date= " << std::fixed << std::setprecision(2) << _Data[date] << std::endl;
-        
-        
     }
     for (std::map<std::string, double>::iterator it = _Data.begin(); it!= _Data.end(); it++){
         std::cout << "date: " << it->first << "   price: " << it->second << std::endl;
